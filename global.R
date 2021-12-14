83746890cb81ba0ebab2f3cc8bde72e8f54f0298
Ignacio Argañaraz <ignacio-96@hotmail.com>
2021-12-14 08:37:03 -0300

    Add global.R

---
+++ global.R
@@ -0,0 +1,37 @@
+
+# Libraries ---------------------------------------------------------------
+
+
+library(shiny)
+library(shinydashboard)
+library(maps)
+
+library(tidyverse)
+library(readr)
+
+
+# Load Data ---------------------------------------------------------------
+
+
+
+
+infrastucture_data<- read_csv("Input/infrastucture_data.csv")
+
+geo_data<- read_csv("Input/geo_data.csv")
+
+states <- map_data("state")
+
+high_cat<-infrastucture_data$high_cat%>%unique()
+sub_cat<-infrastucture_data$category%>%unique()
+
+
+unit<- c("Gross investment","Gross investment at 2012","GDP")
+unit_geo<- c("Gross investment","Gross investment at 2012","GDP","Population","Investment at 2012 per capita")
+
+
+
+# Modules -----------------------------------------------------------------
+
+
+invisible(lapply(list.files(path = "modules", full.names = T), source))
+
