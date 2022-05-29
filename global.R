6a1b94a4457f76606d00a0ef74bc97956b09619f
IgnacioArga <ignacio.arganarazarriazu@gmail.com>
2022-05-29 10:38:39 -0300

    write fix

--- global.R
+++ global.R
@@ -12,12 +12,9 @@ library(readr)
 
 # Load Data ---------------------------------------------------------------
 
+infrastucture_data <- read.csv("Input/infrastucture_data.csv")
 
-
-
-infrastucture_data <- read_csv("Input/infrastucture_data.csv")
-
-geo_data <- read_csv("Input/geo_data.csv")
+geo_data <- read.csv("Input/geo_data.csv")
 
 states <- map_data("state")
 
@@ -27,8 +24,6 @@ sub_cat <- infrastucture_data$category %>% unique()
 
 unit <- c("Gross investment","Gross investment at 2012","GDP")
 unit_geo <- c("Gross investment","Gross investment at 2012","GDP","Population","Investment at 2012 per capita")
-
-
 
 # Modules -----------------------------------------------------------------
 
