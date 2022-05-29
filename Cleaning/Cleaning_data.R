43866541b4589e12a884ea6f689883124f0ddd6b
IgnacioArga <ignacio.arganarazarriazu@gmail.com>
2022-05-29 10:11:13 -0300

    Style

--- Cleaning/Cleaning_data.R
+++ Cleaning/Cleaning_data.R
@@ -1,7 +1,12 @@
-library(readxl)
+
+# Librerías ---------------------------------------------------------------
+
 library(tidyverse)
 
-# Clean gross_inv ----
+# Procesamiento de información --------------------------------------------
+
+# Clean gross_inv 
+
 raw_df  <- readxl::read_excel("Input/Manual/infrastructure-data-may-2020.xlsx", sheet = 2, skip = 2)
 
 gross_inv <- raw_df %>% 
@@ -12,15 +17,14 @@ gross_inv <- raw_df %>%
     group_num = group,
     .after = "category"
   ) %>% 
-  
   fill(meta_cat, group_num) %>%
   pivot_longer(names_to = "year", values_to = "gross_inv", cols = `1947`:`2017`,
                names_transform = list(year = as.integer)) %>% 
   filter(is.na(group)) %>% 
   select(-group)
 
+# Clean gross_inv_chained 
 
-# Clean gross_inv_chained ----
 raw_df2  <- readxl::read_excel("Input/Manual/infrastructure-data-may-2020.xlsx", sheet = 3, skip = 2)
 
 chain_inv <- raw_df2 %>% 
@@ -37,9 +41,8 @@ chain_inv <- raw_df2 %>%
   filter(is.na(group)) %>% 
   select(-group)
 
+# Clean ipd 
 
-
-# Clean ipd ----
 raw_df3  <- readxl::read_excel("Input/Manual/infrastructure-data-may-2020.xlsx", sheet = 4, skip = 2)
 
 ipd <- raw_df3 %>% 
@@ -59,7 +62,8 @@ ipd <- raw_df3 %>%
   select(-group) %>% 
   mutate(meta_cat = if_else(category == "GDP", "GDP", meta_cat))
 
-#Join infrastructure----
+#Join infrastructure
+
 infrastucture_data<-gross_inv%>%
   left_join(chain_inv)%>%
   left_join(ipd)
@@ -74,26 +78,24 @@ infrastucture_data$category<-if_else(str_detect(infrastucture_data$category,"S&L
                                              "Private",
                                              "Federal"))
 
-#Elimino categor�as duplicadas
+#Elimino categor?as duplicadas
+
 conversor<- tribble(
- ~ group_num,~high_cat,
- #- - - - - - - - - - - - - - - - - - - 
-           5,'Total basic infrastructure',
-           6,'Total basic infrastructure',
-           7,'Total basic infrastructure',
-           8,'Total basic infrastructure',
-           11,'Total basic infrastructure',
-           18,'Total social infrastructure',
-           19,'Total social infrastructure',
-           21,'Total social infrastructure',
-           22,'Total digital infrastructure'
+  ~ group_num,~high_cat,
+  #- - - - - - - - - - - - - - - - - - - 
+  5,'Total basic infrastructure',
+  6,'Total basic infrastructure',
+  7,'Total basic infrastructure',
+  8,'Total basic infrastructure',
+  11,'Total basic infrastructure',
+  18,'Total social infrastructure',
+  19,'Total social infrastructure',
+  21,'Total social infrastructure',
+  22,'Total digital infrastructure'
 )
 
-infrastucture_data%>%
-  inner_join(conversor)%>%
-  write.csv("Input/infrastucture_data.csv",row.names = F)
+# Clean geographic data 
 
-# Clean geographic data ----
 geo_gross_inv  <- readxl::read_excel("Input/Manual/infrastructure-data-may-2020.xlsx", sheet = "By state data", skip = 4,n_max = 52) %>% 
   rename(group = ...1, state = ...2)%>%
   gather(`1992`:`2017`,key = year,value = gross_inv)
@@ -120,7 +122,10 @@ geo_total <- geo_gross_inv%>%
 
 geo_total<- geo_total[!str_detect(geo_total$state,"s&l"),]
 
+# Guardo información ------------------------------------------------------
+
 write.csv(geo_total,"Input/geo_data.csv",row.names = F)
 
-
-
+infrastucture_data%>%
+  inner_join(conversor)%>%
+  write.csv("Input/infrastucture_data.csv",row.names = F)
